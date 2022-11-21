276053e94618cb284ed0000824f000d255189ee7
Dima <Semonovdis>
2022-11-21 23:22:00 +0300

    Home Work

---
+++ Example001/Program.cs
@@ -0,0 +1,7 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает
+вторую цифру этого числа.*/
+
+Console.Write("Enter For Number: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string SecondNumber = Convert.ToString(Number);
+Console.WriteLine("The second digit of this number: "+SecondNumber[1]);
---
+++ Example003/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным.*/
+
+Console.Write("Enter a number for the day of the week: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber)
+{
+  if (dayNumber == 6 || dayNumber == 7)
+  {
+  Console.WriteLine("this day is a day off");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) 
+  {
+    Console.WriteLine("7 days in a week!");
+  }
+  else Console.WriteLine("this day is weekday");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
+
+
